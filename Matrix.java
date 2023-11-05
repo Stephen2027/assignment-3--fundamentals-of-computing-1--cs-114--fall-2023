b5f0387071403502113da2e7e07c7e1300ff7230
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-11-05 13:33:48 -0500

    started to create methods

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,24 @@
 public class Matrix {
+  public int[][] matrix;
+  private int size;
+
+  // Constructor
+  public Matrix(int size) {
+    this.size = size;
+    this.matrix = new int[size][size];
+
+    // Print out matrix dimensions
+    System.out.println("Matrix dimensions: " + size + "x" + size);
+  }
+
+  // Populate the matrix with values from 1-(size x size)
+  public void populateMatrix() {
+    int value = 1;
+    for(int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        matrix[row][col] = value;
+        value++;
+      }
+    }
+  }
 }
