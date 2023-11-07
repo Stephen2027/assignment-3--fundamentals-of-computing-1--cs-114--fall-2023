60f739a74071a7964e0203cc9eddc71737156067
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-11-06 22:18:55 -0500

    Finishing touches on methods and began RunMatrix

--- Matrix.java
+++ Matrix.java
@@ -14,10 +14,37 @@ public class Matrix {
   // Populate the matrix with values from 1-(size x size)
   public void populateMatrix() {
     int value = 1;
-    for(int row = 0; row < size; row++) {
+    for (int row = 0; row < size; row++) {
       for (int col = 0; col < size; col++) {
         matrix[row][col] = value;
         value++;
+      }
+    }
+  }
+  // Print matrix diagonals highlighted in yellow
+  public void printMatrix() {
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        if (row + col == size - 1) {
+          System.out.print("\u001B[33m" + matrix[row][col] + "\u001B[0m \t");
+        } else {
+          System.out.print(matrix[row][col] + " \t");
+        }
+      }
+      System.out.println();
+    }
+  }
+  // Private method for swapping matrix elements
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+  // Flip matrix by swapping elements along the diagonal
+  public void flipMatrix() {
+    for (int row = 0; row < size; row++) {
+      for (int col = row + 1; col < size; col++) {
+        swap(row, col, col, row);
       }
     }
   }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    System.out.print("Please enter the size of your matrix:");
+    Scanner scan = new Scanner(System.in);
+    int input = scan.nextInt();
+    Matrix obMatrix = new Matrix(input);
+    obMatrix.printMatrix();
+
+    System.out.print("Populating matrix...");
+    obMatrix.populateMatrix();
+    System.out.println("matrix populated");
+    obMatrix.printMatrix();
+
+    System.out.print("");
   }
 }
